dd6d73aba1d1ee1aacd7c9f1b8604227344a5759
vde-frei <vde-frei@student.42sp.org.br>
2024-01-21 16:42:31 -0300

    updated readme and put lazyfile

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LazyVim-Shell-Script
-Shell Script to install lazyvim and dependencies.
+
+### Shell Script to install lazyvim and dependencies.
+
+I'll update it and insert 42header.
+This script don't need of sudo user to install asdf and dependencies.
+It use asdf pack manager to download all as needed to use it. So, may take several minutes to finish script.
---
+++ install_lazyvim.sh
@@ -0,0 +1,61 @@
+#!bin/bash
+
+# backuping your nvim old configs
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+# clone repository asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
+
+# add path to home and source
+echo ". $HOME/.asdf/asdf.sh" >>~/.zshrc
+source ~/.zshrc
+
+# adding plugins before installing
+asdf plugin add cmake &&
+	asdf plugin add fd &&
+	asdf plugin add neovim &&
+	asdf plugin add nodejs &&
+	asdf plugin add python &&
+	asdf plugin add ruby &&
+	asdf plugin add rust &&
+	asdf plugin add tree-sitter &&
+	asdf plugin add lazygit &&
+	asdf plugin add ripgrep &&
+	asdf install fd 9.0.0 &&
+	asdf install cmake latest &&
+	asdf install neovim stable &&
+	asdf install nodejs 21.5.0 &&
+	asdf install python 3.11.0 &&
+	asdf install ruby 3.0.0 &&
+	asdf install rust latest &&
+	asdf install tree-sitter 0.20.0 &&
+	asdf install ripgrep latest &&
+	asdf install lazygit latest &&
+	asdf global cmake latest &&
+	asdf global neovim stable &&
+	asdf global nodejs 21.5.0 &&
+	asdf global python 3.11.0 &&
+	asdf global ruby 3.0.0 &&
+	asdf global rust latest &&
+	asdf global tree-sitter 0.20.0 &&
+	asdf global ripgrep latest &&
+	asdf global lazygit latest &&
+	pip install neovim &&
+	gem install neovim &&
+	gem environment &&
+	npm install -g neovim
+
+# cloning and setting vim starter configuration
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+# deleting .git directory
+rm -rf ~/.config/nvim/.git
+
+print "Now lazyvim will open and install plugins and setup everything, wait a little bit and them run ':checkhealt'\n to verify if all's fine"
+sleep 5
+
+nvim
